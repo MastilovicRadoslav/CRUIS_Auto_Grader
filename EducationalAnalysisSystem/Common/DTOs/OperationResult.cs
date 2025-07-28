9d8c885e35649788bd1104fb1ae398639bec92f4
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2025-07-28 18:43:51 +0200

    OperationResult

---
+++ EducationalAnalysisSystem/Common/DTOs/OperationResult.cs
@@ -0,0 +1,15 @@
+﻿namespace Common.DTOs
+{
+    public class OperationResult<T>
+    {
+        public bool Success { get; set; }
+        public string? Error { get; set; }
+        public T? Data { get; set; }
+
+        public static OperationResult<T> Ok(T data) =>
+            new OperationResult<T> { Success = true, Data = data };
+
+        public static OperationResult<T> Fail(string error) =>
+            new OperationResult<T> { Success = false, Error = error };
+    }
+}
