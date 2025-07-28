29bbd38a02aac910ea7ad6a88442c22658b4b650
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2025-07-28 18:44:04 +0200

    JwtTokenGenerator

---
+++ EducationalAnalysisSystem/Common/Helpers/JwtTokenGenerator.cs
@@ -0,0 +1,35 @@
+﻿using Common.Enums;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace WebApi.Helpers
+{
+    public static class JwtTokenGenerator
+    {
+        public static string GenerateToken(Guid userId, Role role)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes("your_super_secret_key_1234567890abcd");
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userId.ToString()),
+                new Claim(ClaimTypes.Role, role.ToString())
+            };
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddMinutes(60),
+                Issuer = "EducationalSystem",
+                Audience = "EducationalSystemAudience",
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
