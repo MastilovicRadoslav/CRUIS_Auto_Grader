16e589ad77fa332df834370445d4947cd1cbda96
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2025-07-29 13:46:03 +0200

    DbSettings for MongoDB

---
+++ EducationalAnalysisSystem/Common/Configurations/UserDbSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Common.Configurations
+{
+    public class UserDbSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+        public string DatabaseName { get; set; } = null!;
+        public string CollectionName { get; set; } = null!;
+    }
+}
