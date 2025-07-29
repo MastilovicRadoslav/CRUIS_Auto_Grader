4f6c2901ee35e83b4c987d317144ba4dfb48d363
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2025-07-29 21:23:06 +0200

    IEvaluationService

---
+++ EducationalAnalysisSystem/Common/Interfaces/IEvaluationService.cs
@@ -0,0 +1,19 @@
+﻿using Common.DTOs;
+using Common.Models;
+using Microsoft.ServiceFabric.Services.Remoting;
+using System.ServiceModel;
+using System.Threading.Tasks;
+
+namespace Common.Interfaces
+{
+    [ServiceContract]
+    public interface IEvaluationService : IService
+    {
+        [OperationContract]
+        Task<FeedbackDto> EvaluateAsync(SubmittedWork work); // Čuva radove studenta, obradjuje ih u EvaluationService...
+
+        [OperationContract]
+        Task<List<FeedbackDto>> GetFeedbacksByStudentIdAsync(Guid studentId); // Vraća sve feedback-ove koji pripadaju datom studentu. 
+
+    }
+}
