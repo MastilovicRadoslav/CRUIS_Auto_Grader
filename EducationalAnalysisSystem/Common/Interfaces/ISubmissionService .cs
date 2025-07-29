f6a3d99af577170b2aa1719618c4b938fd5ac147
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2025-07-29 13:46:46 +0200

    ISubmissionService

---
+++ EducationalAnalysisSystem/Common/Interfaces/ISubmissionService .cs
@@ -0,0 +1,18 @@
+﻿using Common.DTOs;
+using Common.Models;
+using Microsoft.ServiceFabric.Services.Remoting;
+using System.ServiceModel;
+
+namespace Common.Interfaces
+{
+    [ServiceContract]
+    public interface ISubmissionService : IService
+    {
+        [OperationContract]
+        Task<OperationResult<Guid>> SubmitWorkAsync(SubmitWorkRequest request);
+
+        [OperationContract]
+        Task<List<SubmittedWork>> GetWorksByStudentIdAsync(Guid studentId);
+    }
+
+}
