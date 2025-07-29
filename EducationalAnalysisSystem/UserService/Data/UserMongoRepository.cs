60a0c85c81371cd904c22ab4807a65b4b6908241
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2025-07-29 13:48:09 +0200

    UserMongoRepository

---
+++ EducationalAnalysisSystem/UserService/Data/UserMongoRepository.cs
@@ -0,0 +1,39 @@
+﻿using Common.Configurations;
+using Common.Models;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
+
+namespace UserService.Data
+{
+    public class UserMongoRepository
+    {
+        private readonly IMongoCollection<User> _users;
+
+        public UserMongoRepository(UserDbSettings settings)
+        {
+            BsonSerializer.RegisterSerializer(new GuidSerializer(GuidRepresentation.Standard));
+
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+            _users = database.GetCollection<User>(settings.CollectionName);
+        }
+
+        public async Task InsertUserAsync(User user)
+        {
+            await _users.InsertOneAsync(user);
+        }
+
+        public async Task<User?> GetByUsernameAsync(string username)
+        {
+            return await _users.Find(u => u.Username == username).FirstOrDefaultAsync();
+        }
+
+        public async Task<List<User>> GetAllUsersAsync()
+        {
+            return await _users.Find(_ => true).ToListAsync();
+        }
+
+    }
+}
