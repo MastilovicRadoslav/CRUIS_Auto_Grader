affc39e62812872d65b4e50c5c2fe806ef051c92
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2025-07-29 21:22:54 +0200

    FeedbackDto

---
+++ EducationalAnalysisSystem/Common/DTOs/FeedbackDto.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Common.DTOs
+{
+    public class FeedbackDto
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        public Guid WorkId { get; set; }
+
+        [BsonRepresentation(BsonType.String)] // 
+        public Guid StudentId { get; set; }
+
+        public int Grade { get; set; }
+        public List<string> Issues { get; set; } = new();
+        public List<string> Suggestions { get; set; } = new();
+        public string Summary { get; set; } = string.Empty;
+    }
+}
