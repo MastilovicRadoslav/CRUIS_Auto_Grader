40df460884491ba477346534237ce1f79be96a7f
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2025-07-29 13:47:07 +0200

    User

--- EducationalAnalysisSystem/Common/Models/User.cs
+++ EducationalAnalysisSystem/Common/Models/User.cs
@@ -1,9 +1,12 @@
 ﻿using Common.Enums;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace Common.Models
 {
     public class User
     {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.String)]
         public Guid Id { get; set; } = Guid.NewGuid();
         public string Username { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
