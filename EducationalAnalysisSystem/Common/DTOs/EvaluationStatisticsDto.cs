b09175253e73fe012419c2afcafdac50a8720a8b
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2025-07-30 10:22:54 +0200

    EvaluationStatisticsDto

---
+++ EducationalAnalysisSystem/Common/DTOs/EvaluationStatisticsDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Common.DTOs
+{
+    public class EvaluationStatisticsDto
+    {
+        public int TotalWorks { get; set; }
+        public double AverageGrade { get; set; }
+        public int Above90 { get; set; }
+        public int Between70And89 { get; set; }
+        public int Below70 { get; set; }
+        public Dictionary<string, int> MostCommonIssues { get; set; } = new();
+    }
+
+}
