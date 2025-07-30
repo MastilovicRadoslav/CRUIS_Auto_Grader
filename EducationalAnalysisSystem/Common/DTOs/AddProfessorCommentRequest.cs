15d3276463778ed761c44d6f1e05e6c117db4ee3
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2025-07-30 10:22:41 +0200

    AddProfessorCommentRequest

---
+++ EducationalAnalysisSystem/Common/DTOs/AddProfessorCommentRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Common.DTOs
+{
+    public class AddProfessorCommentRequest
+    {
+        public Guid WorkId { get; set; }
+        public string Comment { get; set; } = string.Empty;
+    }
+
+}
