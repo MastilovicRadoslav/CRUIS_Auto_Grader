9f7f069dfbfc43322a3ff58c9517ff17ce9e3c6c
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2025-07-31 12:48:24 +0200

    App.jsx

--- educational-frontend/src/App.jsx
+++ educational-frontend/src/App.jsx
@@ -1,18 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import Navbar from "./components/Navbar";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
-import { AuthProvider } from "./context/AuthContext";
-
+import StudentDashboard from "./pages/StudentDashboard";
+import { Layout } from "antd";
 
 function App() {
   return (
     <AuthProvider>
       <Router>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          {/* Ostale rute za studenta, profesora, admina dolaze kasnije */}
-        </Routes>
+        <Navbar />
+        <Layout style={{ marginTop: 64, minHeight: "100vh", backgroundColor: "#f0f2f5" }}>
+          <div style={{ maxWidth: 1200, margin: "0 auto", padding: "20px" }}>
+            <Routes>
+              <Route path="/" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route path="/student" element={<StudentDashboard />} />
+              {/* Ostale rute za profesora i admina dolaze kasnije */}
+            </Routes>
+          </div>
+        </Layout>
       </Router>
     </AuthProvider>
   );
