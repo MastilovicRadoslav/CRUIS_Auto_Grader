23bca15c57a73fef86dcd7cecba0300555b27b42
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2025-08-01 13:11:45 +0200

    submissionService

--- educational-frontend/src/services/submissionService.js
+++ educational-frontend/src/services/submissionService.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
+// Dobavljanje svih radova za student a
 export const fetchMySubmissions = async (token) => {
   const response = await axios.get(`${API_URL}/submission/my`, {
     headers: {
@@ -11,19 +12,34 @@ export const fetchMySubmissions = async (token) => {
   return response.data;
 };
 
+// Dodavanje rada od nekog studenta
 export const submitWork = async (payload, token) => {
   const response = await axios.post(`${API_URL}/submission/submit`, payload, {
-    headers: { Authorization: `Bearer ${token}` },
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
   });
+
   return response.data;
 };
 
+// Dobavljanje evaluacije za odredjeni rad
 export const getFeedbackByWorkId = async (workId) => {
   const token = localStorage.getItem("token");
-  const res = await axios.get(`${API_URL}/evaluation/feedback/my/${workId}`, {
+  const res = await axios.get(`${API_URL}/evaluation/feedback/${workId}`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
   });
   return res.data;
+};
+
+//Dobavljanje svih radova za sve studente - Professor
+export const fetchAllSubmissions = async (token) => {
+  const response = await axios.get(`${API_URL}/submission/all`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
 };
