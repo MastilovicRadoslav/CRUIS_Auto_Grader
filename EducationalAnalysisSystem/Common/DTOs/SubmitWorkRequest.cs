d88aef63ef0dcb9eea48243c9b771a59950c02f7
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2025-08-01 13:13:03 +0200

    SubmitWorkRequest

--- EducationalAnalysisSystem/Common/DTOs/SubmitWorkRequest.cs
+++ EducationalAnalysisSystem/Common/DTOs/SubmitWorkRequest.cs
@@ -3,7 +3,7 @@
     public class SubmitWorkRequest
     {
         public Guid StudentId { get; set; }
-        public string Content { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty; // Kasnije ce biti za razlicite fajlove
         public string Title { get; set; } = string.Empty;
     }
 }
