dbc4824182a7d9a5de042ed75d02592bba12dc40
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2025-08-02 17:16:18 +0200

    AnalysisResultDto
    LLM Ollama returns this Dto model to me, it can be adjusted as we wish

---
+++ EducationalAnalysisSystem/Common/DTOs/AnalysisResultDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Common.DTOs
+{
+    public class AnalysisResultDto
+    {
+        public int Grade { get; set; }
+        public List<string> Issues { get; set; } = new();
+        public List<string> Suggestions { get; set; } = new();
+        public string Summary { get; set; } = string.Empty;
+    }
+
+}
