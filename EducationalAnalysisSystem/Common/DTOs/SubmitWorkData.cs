7f8c3235dd170133ad1a9a20926ac139dc042bcd
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2025-08-02 17:16:50 +0200

    SubmitWorkData
    A class of needs for manipulating data through services

---
+++ EducationalAnalysisSystem/Common/DTOs/SubmitWorkData.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Serialization;
+
+namespace Common.DTOs
+{
+    [DataContract]
+
+    public class SubmitWorkData
+    {
+        [DataMember]
+        public Guid StudentId { get; set; }
+        [DataMember]
+        public string Title { get; set; } = string.Empty;
+        [DataMember]
+        public string Content { get; set; } = string.Empty;
+        [DataMember]
+        public AnalysisResultDto Analysis { get; set; } = new();
+    }
+
+}
